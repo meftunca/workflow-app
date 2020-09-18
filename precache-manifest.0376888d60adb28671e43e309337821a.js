b0bc2f03cecadcd30440efeecbb6f938ea8b875d
meftunca <buraksenturk25@gmail.com>
2020-09-18 13:20:35 +0300

    Updates

---
+++ precache-manifest.0376888d60adb28671e43e309337821a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6c2d30e0a9f781fcbe36a6ec75e9f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1716c8ab2ebcc164b9a5",
+    "url": "./static/css/main.f037933c.chunk.css"
+  },
+  {
+    "revision": "475151a9d5dab39fd8ab",
+    "url": "./static/js/2.af197920.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.af197920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1716c8ab2ebcc164b9a5",
+    "url": "./static/js/main.58addc30.chunk.js"
+  },
+  {
+    "revision": "28d818269d3f773f0e38",
+    "url": "./static/js/runtime-main.d20ff0f7.js"
+  }
+]);
